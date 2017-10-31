219beaa23fec69d19963043606e75e84954eabee
lenington2 <31635763+lenington2@users.noreply.github.com>
2017-10-31 17:20:20 -0500

    ACTUALIZA

---
+++ ACTIVIDADES.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Meteorologia</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body background="fondo.jpg">
+<body>
+
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        
+                    </a>
+                </li>
+                <li>
+                    <a href="LAMINA/consulta.php">CDA La Mina</a>
+                </li>
+                <li>
+                    <a href="APTOPTOBOLIVAR/consulta.php">Aeropuerto Puerto Bolívar</a>
+                </li>
+                <li>
+                    <a href="HDALACRUZ/consulta.php">Hacienda Santa Cruz</a>
+                </li>
+                <li>
+                    <a href="PITSUR/consulta.php">Tajo Sur</a>
+                </li>
+                 <li>
+                    <a href="HDALAESPERANZA/consulta.php">Hacienda La Esperanza</a>
+                </li>
+                <li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        
+        
+        
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Estaciones Meteorológicas</h1>
+                        <p>Las estaciones meteorológicas instaladas según convenio IDEAM - CERREJON, se encuentran categorizadas  de la siguiente forma :</p>
+                        <p>CP CLIMATOLÓGICA PRINCIPAL : en esta categoría se encuentra instalada la estación PTO BOLIVAR, CDA LA MINA  con los siguientes instrumentos: </p>
+                        <p>Pluviómetro, Pluviografo, Termómetro de Máxima, Termómetro de Mínima, Termómetro Seco, Termómetro Húmedo, Termohigrografo, Tanque de evaporación, Anemómetro, Anemocinemografo, Heliógrafo.</p>
+                        <p>ME METEOROLÓGICA ESPECIAL : en esta categoría se encuentra instalada la estación HDA LA CRUZ  con los siguientes instrumentos: </p>
+                        <p>Pluviómetro, Tanque Evaporación</p>
+                        <p>PG PLUVIOGRÁFICAS : en esta categoría se encuentra instalada la estación TAJO SUR, HDA LA ESPERANZA  con los siguientes instrumentos: </p>
+                        <p>Pluvigrafo</p>
+                        <dl class="dl-horizontal">
+  <dt>Ingeniero Responsable</dt>
+  <dd>Eliana Maria Gomez Sanchez</dd>
+  <dt></dt>
+  <dd>Ingeniero Sanitario y Ambiental</dd>
+  <dt></dt>
+  <dd>IDEAM Area Operativa 05 Santa Marta</dd>
+  <dt></dt>
+  <dd>Tel. 4306766-4306565</dd>
+  <dt></dt>
+ <dd>emgomez@ideam.gov.co</dd>
+  </dl>
+  
+   <a href="/" target="_self"><input type="submit" class="btn btn-primary"
+                       value="Regresar">
+                       </form></div>
+                         </div>
+ 
+    <br>
+   
+                     </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+    
+    
+    <div class="container text-center">
+<p class="bg-primary">Proyecto Realizado Por Lenington Cantillo. Tecnico Adm.  IDEAM Area Ope 02 Barranquilla  </p>
+</body>
+
+</html>
